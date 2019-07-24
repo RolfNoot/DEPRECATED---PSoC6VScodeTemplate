9e4ba84de9b46ffbc4856fb6910bb514a26ec6ef
Rolf <rolf@rolf-mac-pro-2.home>
2019-07-24 15:57:38 +0200

    Changed gitignore and testing it

--- build/CMakeFiles/PreBuild.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/PreBuild"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/PreBuild.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/VStest4.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/VStest4"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/VStest4.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
